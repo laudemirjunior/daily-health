1d496b80c89d614c86dca1cbf8fe4f128aec08d0
Laudemir <nlaudemir@gmail.com>
2021-10-14 10:29:51 -0300

    initial commit

---
+++ src/components/hamburguer/index.jsx
@@ -0,0 +1,5 @@
+const Hamburguer = () => {
+  return <div></div>;
+};
+
+export default Hamburguer;
---
+++ src/styles/index.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    border: none;
+    font-family: 'Nunito', sans-serif;
+  }
+  .App {
+      width: 100%;
+      min-height: 100vh;
+  }
+  :root {
+    --white: #ffffff
+    --black: #000000
+    --button: #F2A0AF;
+    --background: #FD7FAA;
+    ${"" /* usar opacity: opacity: 0.8; no efeitos transversais: */}
+    --bar: #F2527D;
+  }
+  button {
+    cursor: pointer;
+  }
+  `;
+
+export default GlobalStyle;
