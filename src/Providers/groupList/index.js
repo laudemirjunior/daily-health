d0869f87d335d1c712005908d56e20455641b895
Laudemir <nlaudemir@gmail.com>
2021-10-17 15:15:10 -0300

    Providers goals and activities

--- src/Providers/groupList/index.js
+++ src/Providers/groupList/index.js
@@ -1,31 +1,26 @@
 import { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import api from "../../services";
 export const GroupListContext = createContext();
 
 export const GroupListProvider = ({ children }) => {
   const [groupList, setgroupList] = useState([]);
-  const [link, setLink] = useState(
-    "https://kenzie-habits.herokuapp.com/groups/"
-  );
+  const token = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM0NzY2MTQzLCJqdGkiOiI4ZmFkOGU4ODU1OGI0ZGFiOGJlZGI1YWNhYTYxOWQwMiIsInVzZXJfaWQiOjE1fQ.MyM-dshWnP1BhPl-jbGWJGvTpe_ujZzKuEN1N6so-pY`;
 
   useEffect(() => {
-    if (link !== null) {
-      axios
-        .get(`${link}`, {
-          headers: {
-            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM0NzY2MTQzLCJqdGkiOiI4ZmFkOGU4ODU1OGI0ZGFiOGJlZGI1YWNhYTYxOWQwMiIsInVzZXJfaWQiOjE1fQ.MyM-dshWnP1BhPl-jbGWJGvTpe_ujZzKuEN1N6so-pY`,
-          },
-        })
-        .then((response) => {
-          setgroupList([...groupList, ...response.data.results]);
-          setLink(response.data.next);
-        })
-        .catch((err) => console.log(err));
-    }
+    api
+      .get("/groups/", {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        setgroupList(response.data.results);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <GroupListContext.Provider value={{ groupList, setgroupList }}>
+    <GroupListContext.Provider value={{ groupList }}>
       {children}
     </GroupListContext.Provider>
   );
