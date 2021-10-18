f0971d1f42ce2a94e7674f6cbbc06953f40af63d
Laudemir <nlaudemir@gmail.com>
2021-10-18 14:17:12 -0300

    complet correction

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -4,12 +4,7 @@ export const StyleButton = styled.button`
   width: 250px;
   min-height: 50px;
   background-color: var(--button);
-<<<<<<< HEAD
   border-radius: 10px;
-=======
-  color: var(--white);
-  border-radius: 20px;
->>>>>>> origin/feature/page_login
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
   justify-content: center;
